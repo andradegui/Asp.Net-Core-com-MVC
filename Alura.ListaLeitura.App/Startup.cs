b8e5fad14d90b2afb0a80517ccb352b1ddc34d76
Guilherme Lima <glima@noovi.com.br>
2021-09-01 18:29:32 -0300

    Projeto do curso Asp.Net Core usando Wep app com MVC

---
+++ Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Alura.ListaLeitura.App.Logica;
+using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
+{
+    public class Startup
+    {  
+
+       public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+            services.AddMvc();
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseMvcWithDefaultRoute();
+
+        }
+    }
+}
